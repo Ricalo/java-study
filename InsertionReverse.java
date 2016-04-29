2ef36c903ef8b8b9f057dbc47ceeef5fec4683ae
Ricardo Loo <ricardol@microsoft.com>
2016-04-28 17:25:52 -0700

    Adding inverse of insertion sort

---
+++ InsertionReverse.java
@@ -0,0 +1,14 @@
+public class InsertionReverse {
+    public static int[] sort(int[] array) {
+        for(int j = 1; j < array.length; j++) {
+            int key = array[j];
+	    int i = j - 1;
+	    while(i >= 0 && array[i] < key) {
+                array[i + 1] = array[i];
+		i = i - 1;
+	    }
+	    array[i + 1] = key;
+	}
+	return array;
+    }
+}
