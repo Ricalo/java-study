daf8c8e3d1d5fb3396aa2779939f54176dd515b5
Ricardo Loo <ricardol@microsoft.com>
2016-05-05 21:52:55 -0700

    Adding MergeSort

---
+++ MergeSort.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void sort(int[] array) {
+	System.out.println("Splitting " + Arrays.toString(array));
+        if(array.length > 1) {
+	    int mid = array.length / 2;
+            int[] left = Arrays.copyOfRange(array, 0, mid);
+	    int[] right = Arrays.copyOfRange(array, mid, array.length);
+
+	    sort(left);
+	    sort(right);
+
+	    int i = 0;
+	    int j = 0;
+	    int resultIndex = 0;
+
+	    while(i < left.length && j < right.length) {
+                if(left[i] < right[j]) {
+	            array[resultIndex] = left[i];
+		    i++;
+		    resultIndex++;
+		} else {
+                    array[resultIndex] = right[j];
+		    j++;
+		    resultIndex++;
+		}
+	    }
+
+	    while(i < left.length) {
+                array[resultIndex] = left[i];
+		i++;
+		resultIndex++;
+	    }
+
+	    while(j < right.length) {
+                array[resultIndex] = right[j];
+		j++;
+		resultIndex++;
+	    }
+	}
+	System.out.println("Merging " + Arrays.toString(array));
+    }
+}
--- Runner.java
+++ Runner.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
         int[] array = {5, 2, 4, 6, 1, 3};
-	printArray(array);
-	InsertionSort.sort(array);
-	printArray(array);
+	System.out.println(Arrays.toString(array));
+	MergeSort.sort(array);
+	System.out.println(Arrays.toString(array));
     }
 
     public static void printArray(int[] array) {
