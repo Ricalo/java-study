8706a474ec42445f135d8283b1dc0ad3789f1e09
Ricardo Loo <ricardol@microsoft.com>
2016-05-06 12:50:26 -0700

    Adding BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void sort(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+	    for(int j = array.length - 1; j > i; j--) {
+		if(array[j] < array[j - 1]) {
+	            int temp;
+		    temp = array[j];
+                    array[j] = array[j - 1];
+		    array[j - 1] = temp;
+		}
+	    }
+	    System.out.println("Sorting " + Arrays.toString(array));
+	}
+    }
+}
--- Runner.java
+++ Runner.java
@@ -4,14 +4,7 @@ public class Runner {
     public static void main(String[] args) {
         int[] array = {5, 2, 4, 6, 1, 3};
 	System.out.println(Arrays.toString(array));
-	MergeSort.sort(array);
+	BubbleSort.sort(array);
 	System.out.println(Arrays.toString(array));
-    }
-
-    public static void printArray(int[] array) {
-        for(int i  = 0; i < array.length; i++) {
-            System.out.print(array[i] + ", ");
-	}
-	System.out.println();
     }
 }
