df2f587624fcdbffd15a0ac43841116b59ef0e85
Ricardo Loo <ricalox@gmail.com>
2017-02-26 14:53:22 -0800

    Adding gradle and gradlew to the project
    * Ran gradle init --type java-library
    * Moved source files to src/main/java folder

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static void sort(int[] array) {
+    public static int[] sort(int[] array) {
         for(int i = 0; i < array.length; i++) {
 	    for(int j = array.length - 1; j > i; j--) {
 		if(array[j] < array[j - 1]) {
@@ -11,7 +11,8 @@ public class BubbleSort {
 		    array[j - 1] = temp;
 		}
 	    }
-	    System.out.println("Sorting " + Arrays.toString(array));
 	}
+
+        return array;
     }
 }
---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BubbleSortTest {
+    @Test
+    public void testAlreadySorted() {
+        int[] data = {1, 2, 3, 4};
+	int[] sortedData = BubbleSort.sort(data);
+        assertTrue("Bubble sort works with already sorted arrays", sortedData[0] == 1);
+        assertTrue("Bubble sort works with already sorted arrays", sortedData[3] == 4);
+    }
+
+    @Test
+    public void testWorstScenario() {
+        int[] data = {4, 3, 2, 1};
+	int[] sortedData = BubbleSort.sort(data);
+        assertTrue("Bubble sort works with arrays in reverse order", sortedData[0] == 1);
+        assertTrue("Bubble sort works with arrays in reverse order", sortedData[3] == 4);
+    }
+}
