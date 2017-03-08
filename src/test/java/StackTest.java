c6f7eef24de7dc953635cd9b8ef74177526cbbed
Ricardo Loo <rloo@google.com>
2017-03-08 02:18:51 -0800

    Added test for 1000 elements in the stack

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -6,17 +6,28 @@ public class StackTest {
     public void testOneElement() {
         Stack stack = new Stack();
 
-	int pushed = 1;
-	stack.push(pushed);
+        int pushed = 1;
+        stack.push(pushed);
         
-	int popped = stack.pop();
-	assertTrue("We should get what we pushed into the stack", popped == pushed);
+        int popped = stack.pop();
+        assertTrue(
+            "We should get what we pushed into the stack", popped == pushed
+        );
     }
     
     @Test(expected = NullPointerException.class)
     public void testPopEmptyStack() {
 	Stack stack = new Stack();
 	stack.pop();
+    }
+
+    @Test
+    public void testThousandElements() {
+        Stack stack = new Stack();
+
+        for(int i = 0; i < 1000; i++) {
+            stack.push(i);
+        }
     }
 }
 	
