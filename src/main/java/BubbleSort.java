7ef319ec5767045fbb735f7b0ca9df5700452d3c
Ricardo Loo <ricalox@gmail.com>
2017-04-09 08:53:55 -0700

    Added @NonNull annotation for sort parameter

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,7 +1,8 @@
+import org.checkerframework.checker.nullness.qual.*;
 import java.util.Arrays;
 
 public class BubbleSort {
-    public static int[] sort(int[] array) {
+    public static int[] sort(@NonNull int[] array) {
         for(int i = 0; i < array.length; i++) {
 	        for(int j = array.length - 1; j > i; j--) {
     		    if(array[j] < array[j - 1]) {
