596726a7baa055953b19cc052db8b05570b45f12
Ricardo Loo <ricalox@gmail.com>
2017-04-10 20:19:31 -0700

    Fixed indenting

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 public class BubbleSort {
     public static int[] sort(@NonNull int[] array) {
         for(int i = 0; i < array.length; i++) {
-	        for(int j = array.length - 1; j > i; j--) {
-    		    if(array[j] < array[j - 1]) {
-	                int temp;
-    		        temp = array[j];
+            for(int j = array.length - 1; j > i; j--) {
+                if(array[j] < array[j - 1]) {
+                    int temp;
+                    temp = array[j];
                     array[j] = array[j - 1];
-        	        array[j - 1] = temp;
-	     	    }
-	        }
-	    }
+                    array[j - 1] = temp;
+                }
+            }
+        }
         return array;
     }
 }
