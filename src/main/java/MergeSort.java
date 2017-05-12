7db35e1e8bf5f0429fa5b1da125086bc2e21b6b7
Ricardo Loo <ricalox@gmail.com>
2017-05-11 22:34:15 -0700

    Fixing indentation of MergeSort

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -2,43 +2,43 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void sort(int[] array) {
-	System.out.println("Splitting " + Arrays.toString(array));
+        System.out.println("Splitting " + Arrays.toString(array));
         if(array.length > 1) {
-	    int mid = array.length / 2;
+            int mid = array.length / 2;
             int[] left = Arrays.copyOfRange(array, 0, mid);
-	    int[] right = Arrays.copyOfRange(array, mid, array.length);
+            int[] right = Arrays.copyOfRange(array, mid, array.length);
 
-	    sort(left);
-	    sort(right);
+            sort(left);
+            sort(right);
 
-	    int i = 0;
-	    int j = 0;
-	    int resultIndex = 0;
+            int i = 0;
+            int j = 0;
+            int resultIndex = 0;
 
-	    while(i < left.length && j < right.length) {
+            while(i < left.length && j < right.length) {
                 if(left[i] < right[j]) {
-	            array[resultIndex] = left[i];
-		    i++;
-		    resultIndex++;
-		} else {
+                    array[resultIndex] = left[i];
+                    i++;
+                    resultIndex++;
+                } else {
                     array[resultIndex] = right[j];
-		    j++;
-		    resultIndex++;
-		}
-	    }
+                    j++;
+                    resultIndex++;
+                }
+            }
 
-	    while(i < left.length) {
+            while(i < left.length) {
                 array[resultIndex] = left[i];
-		i++;
-		resultIndex++;
-	    }
+                i++;
+                resultIndex++;
+            }
 
-	    while(j < right.length) {
+            while(j < right.length) {
                 array[resultIndex] = right[j];
-		j++;
-		resultIndex++;
-	    }
-	}
-	System.out.println("Merging " + Arrays.toString(array));
+                j++;
+                resultIndex++;
+            }
+        }
+    System.out.println("Merging " + Arrays.toString(array));
     }
 }
