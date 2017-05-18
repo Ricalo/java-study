6ea2aa00963153bcc8f9442439a12285af652f00
Ricardo Loo <ricalox@gmail.com>
2017-05-18 10:55:30 -0700

    Replacing tabs with spaces

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -4,15 +4,16 @@ public class InsertionSort {
     public static int[] sort(int[] array) {
         for(int j = 1; j < array.length; j++) {
             int key = array[j];
-	    System.out.println("Sorting key: " + key);
-	    int i = j - 1;
-	    while(i >= 0 && array[i] > key) {
-                array[i + 1] = array[i];
-		i = i - 1;
-	    }
-	    array[i + 1] = key;
-	    System.out.println("Sorting " + Arrays.toString(array));
-	}
-	return array;
+        System.out.println("Sorting key: " + key);
+        int i = j - 1;
+        while(i >= 0 && array[i] > key) {
+            array[i + 1] = array[i];
+            i = i - 1;
+        }
+        array[i + 1] = key;
+        System.out.println("Sorting " + Arrays.toString(array));
+    }
+    return array;
     }
 }
+
