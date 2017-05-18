fdafc2174ad393f155cc978b959c122e15938f68
Ricardo Loo <ricalox@gmail.com>
2017-05-17 23:22:20 -0700

    Replaced tabs with spaces

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -5,29 +5,29 @@ public class Stack {
         if(head == null) {
             throw new NullPointerException("The stack is empty");
         } else {
-	    int temp = head.data;
-	    head = head.next;
-	    return temp;
-	}
+            int temp = head.data;
+            head = head.next;
+            return temp;
+        }
     }
 
     public void push(int data) {
         ListElement newElement = new ListElement();
-	newElement.data = data;
+        newElement.data = data;
 
-	newElement.next = head;
-	head = newElement;
+        newElement.next = head;
+        head = newElement;
     }
 
     @Override
     public String toString() {
-	ListElement current = head;
-	String returnValue = "";
+        ListElement current = head;
+        String returnValue = "";
         while(current != null) {
             returnValue = returnValue + current.data + "\n";
-	    current = current.next;
-	}
+            current = current.next;
+        }
 
-	return returnValue;
+        return returnValue;
     }
 }
