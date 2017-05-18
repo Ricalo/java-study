c8124992c7cbf558046517074170125e5aa90576
Ricardo Loo <ricalox@gmail.com>
2017-05-18 16:56:46 -0700

    Replaced tabs with spaces

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class Factorial {
     public static int execute(int value) {
         if(value <= 1) {
-	    return 1;
-	} else {
+        return 1;
+        } else {
             return execute(value - 1) * value;
-	}
+        }
     }
 }
+
