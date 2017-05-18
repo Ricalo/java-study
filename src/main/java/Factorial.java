337deda5a39d5b1f77d528a2b5afc946c3567fbf
Ricardo Loo <ricalox@gmail.com>
2017-05-18 16:58:08 -0700

    Fixed indentation

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -3,7 +3,7 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 public class Factorial {
     public static int execute(@NonNull int value) {
         if(value <= 1) {
-        return 1;
+            return 1;
         } else {
             return execute(value - 1) * value;
         }
