879743f25bc26af99e50c7cbbab2df6fb6b8c230
Ricardo Loo <ricalox@gmail.com>
2017-05-18 17:21:48 -0700

    factorial handles negative numbers with an exception

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -2,7 +2,10 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class Factorial {
     public static int execute(@NonNull int value) {
-        if(value <= 1) {
+        if(value < 0) {
+            throw new ArithmeticException("Factorial doesn't accept negative" +
+                    "numbers");
+        } else if(value == 0) {
             return 1;
         } else {
             return execute(value - 1) * value;
