f0b82c36a1551f133b236d8b0f3c40aee2b4bc26
Ricardo Loo <ricalox@gmail.com>
2017-09-18 21:32:55 -0700

    Making sure not to modify original array before sorting it

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -3,18 +3,29 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static int[] sort(@NonNull int[] array) {
-        for(int j = 1; j < array.length; j++) {
-            int key = array[j];
-        System.out.println("Sorting key: " + key);
-        int i = j - 1;
-        while(i >= 0 && array[i] > key) {
-            array[i + 1] = array[i];
-            i = i - 1;
+        if(array.length < 2) {
+            return array;
         }
-        array[i + 1] = key;
-        System.out.println("Sorting " + Arrays.toString(array));
-    }
-    return array;
+
+        // Make a clone of the initial array to preserve the original order
+        int[] ret = array.clone();
+
+        System.out.println("      Input: " + Arrays.toString(array));
+
+        for(int j = 1; j < ret.length; j++) {
+            int key = ret[j];
+            System.out.println("Sorting key: " + key);
+            int i = j - 1;
+            while(i >= 0 && ret[i] > key) {
+                ret[i + 1] = ret[i];
+                i = i - 1;
+            }
+            ret[i + 1] = key;
+            System.out.println("Sorting " + Arrays.toString(ret));
+        }
+
+        System.out.println("     Output: " + Arrays.toString(ret));
+        return ret;
     }
 }
 
