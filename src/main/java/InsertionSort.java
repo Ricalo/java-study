f20d28b0dbd33b9e37bee3bd5274a46ba078cca6
Ricardo Loo <ricalox@gmail.com>
2017-09-29 07:10:56 -0700

    Aligned "Sorting" printing

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -9,14 +9,15 @@ public class InsertionSort extends BaseArraySort {
 
         for(int j = 1; j < array.length; j++) {
             int key = array[j];
-            System.out.println("Sorting key: " + key);
+            System.out.println("Sorting key: " + getPaddingForArray(array, j)
+                    + " " + key);
             int i = j - 1;
             while(i >= 0 && array[i] > key) {
                 array[i + 1] = array[i];
                 i = i - 1;
             }
             array[i + 1] = key;
-            System.out.println("Sorting " + Arrays.toString(array));
+            System.out.println("    Sorting: " + Arrays.toString(array));
         }
     }
 }
