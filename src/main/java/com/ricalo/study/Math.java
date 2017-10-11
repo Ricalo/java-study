276355936f71ae6753c534fa577d884791725b50
Ricardo Loo <ricalox@gmail.com>
2017-10-11 10:23:41 -0700

    Cleaning Math class of lints from PMD

--- src/main/java/com/ricalo/study/Math.java
+++ src/main/java/com/ricalo/study/Math.java
@@ -2,8 +2,12 @@ package com.ricalo.study;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-public class Math {
-    public static int factorial(@NonNull int value) {
+public final class Math {
+    private Math() {
+        throw new AssertionError("Instantiating a utility class");
+    }
+
+    public static int factorial(@NonNull final int value) {
         if(value < 0) {
             throw new ArithmeticException("Factorial doesn't accept negative numbers");
         } else if(value == 0) {
@@ -13,7 +17,7 @@ public class Math {
         }
     }
 
-    public static double toPowerRecursive(@NonNull long base, @NonNull int power) {
+    public static double toPowerRecursive(@NonNull final long base, @NonNull final int power) {
         if(power == 0) {
             return 1;
         } else if (power < 0) {
@@ -27,22 +31,20 @@ public class Math {
         }
     }
 
-    public static double toPowerIterative(@NonNull long base, @NonNull int power) {
-        double ret=1;
-        boolean negativePower = power < 0;
+    public static double toPowerIterative(@NonNull final long base, @NonNull final int power) {
+        double ret = 1;
+        final boolean negativePower = power < 0;
+        int absolutePower = negativePower ? power * -1 : power;
+        long tempBase = base;
 
-        if(negativePower) {
-            power = power * -1;
-        }
-
-        int i = 0;
-        while (i < power) {
-            if(power % 2 == 0) {
-                power = power / 2;
-                base = base * base;
+        int index = 0;
+        while (index < absolutePower) {
+            if(absolutePower % 2 == 0) {
+                absolutePower = absolutePower / 2;
+                tempBase = tempBase * tempBase;
             } else {
-                ret = ret * base;
-                i++;
+                ret = ret * tempBase;
+                index++;
             }
         }
 
