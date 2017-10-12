ebfb79e33378e6526a60afd100a4a471d33c6ab6
Ricardo Loo <ricalox@gmail.com>
2017-10-11 22:24:06 -0700

    Cleaned StackTest from PMD lints

--- src/test/java/com/ricalo/study/StackTest.java
+++ src/test/java/com/ricalo/study/StackTest.java
@@ -8,46 +8,46 @@ import static org.junit.Assert.*;
 public class StackTest {
     @Test
     public void testOneElement() {
-        Stack stack = new Stack();
+        final Stack stack = new Stack();
 
-        int pushed = 1;
+        final int pushed = 1;
         stack.push(pushed);
         
-        int popped = stack.pop();
-        assertTrue(popped == pushed
-        );
+        final int popped = stack.pop();
+        assertSame(popped, pushed);
     }
     
     @Test(expected = EmptyStackException.class)
     public void testPopEmptyStack() {
-        Stack stack = new Stack();
+        final Stack stack = new Stack();
         stack.pop();
     }
 
     @Test
     public void testThousandElements() {
-        Stack stack = new Stack();
+        final Stack stack = new Stack();
 
         for(int i = 0; i < 1000; i++) {
             stack.push(i);
         }
     }
 
+    @SuppressWarnings("PMD.LawOfDemeter")
     @Test
     public void testToStringEmptyStack() {
-        Stack stack = new Stack();
+        final Stack stack = new Stack();
 
-        assertTrue(stack.toString().length() == 0);
+        assertTrue(stack.toString().isEmpty());
     }
 
+    @SuppressWarnings("PMD.LawOfDemeter")
     @Test
     public void testToStringOneElement() {
-        Stack stack = new Stack();
+        final Stack stack = new Stack();
 
-        int pushed = 1;
+        final int pushed = 1;
         stack.push(pushed);
 
-        assertTrue(stack.toString().equals(String.valueOf(pushed) + "\n"));
+        assertEquals(stack.toString(), pushed + "\n");
     }
 }
-
